12c8c4271fa31bb717720f23b1567243d5b630f0
Hafid Eledath <105419944+hafideledath@users.noreply.github.com>
2026-03-18 17:38:25 +0400

    tweaked music mapping

--- scripts.js
+++ scripts.js
@@ -13,7 +13,7 @@
     };
 
     const sectionMap = [
-        { selector: '.cover-container, .opener, .prologue',    src: 'music/prologue.mp3' },
+        { selector: '.opening-music-container',    src: 'music/prologue.mp3' },
         { selector: '.movement-i',  src: 'music/movement1.mp3' },
         { selector: '.movement-ii', src: 'music/movement2.mp3' },
         { selector: '.movement-iii',src: 'music/movement3.mp3' },
